e8344ca03df8303a0e49702c7e5f0705948ab8ed
gabriel zinetti <101438268+GabrielZinetti@users.noreply.github.com>
2023-05-02 17:11:31 -0300

    Update createFile.js

--- createFile.js
+++ createFile.js
@@ -1,9 +1,5 @@
 let fs = require('fs');
 
-/* Using the funtion writeFile to create the file
- * we can create file using other function, like writeStream and write too
- * we can create the file by passing a text or a String variable, in the place of "Some Text.."
- */
 fs.writeFile("./Texto/text2.txt",'Another text', function(err){
 	if(err){
 		return console.log('error')
